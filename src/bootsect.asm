edb360fcf6e5007b92547d0774e9ec68ba2f6716
insung3511 <insung.park123@gmail.com>
2022-08-23 16:09:42 +0900

    Added Boot Code, Boot loader

--- src/bootsect.asm
+++ src/bootsect.asm
@@ -29,7 +29,7 @@ db  0
 db  029h
 dd  0
 db  'RMOS BIN'
-db  'FAT12'
+db  'FAT12      '
 
 ; ====================
 ; main bootsector code
@@ -64,6 +64,40 @@ entry:
 
         jmp INITSEG:main
 
+;========================
+; Read sector
+;========================
+%define SECTORS_PER_TRACK       18
+%define SECTORS_OF_TWO_TRACK    SECTORS_PER_TRACK * 2
+
+proc    read_sector
+        mov ax, 1
+        pusha
+
+.init:
+        mov ax, word [bp+4]
+
+        mov bl, SECTORS_OF_TWO_TRACK
+        div bl  ; division with ax, bl
+                ; quotient will moved to al, remainder will be moved to 
+        mov ch, al      ; move track
+        shr ax, 8
+
+        ; get "head" number
+        mov bl, SECTORS_PER_TRACK
+        mov dl, 00h
+        mov es, word[bp+6]
+        mov bx, word[bp+8]
+        int 13h
+
+        popa
+
+        jnc .out
+        xor ax, ax
+
+.out:
+endproc
+
 main:
         mov ax, INITSEG
         mov ds, ax
@@ -88,6 +122,54 @@ main:
         mov dl, 00h
         int 13h
 
+        ; read FAT & Directory table
+        mov cx, 9+14
+        mov bx, 0000h
+        mov dx, 0ah
+
+.read_fat_dir:
+        push bx
+        push FATSEG
+        push dx
+        call read_sector
+
+        add sp, 6
+        or ax, ax
+        jnz .read_next_fat_dir
+        call sys_halt
+
+.read_next_fat_dir:
+        inc dx
+        add bx, 512
+        loop .read_fat_dir
+
+        mov cx, 224
+        mov bx, 1400h
+
+;=====================
+; sys_halt
+;=====================
+proc    sys_halt
+        push msg_error
+        call msg_out
+        add sp, 2
+
+.infinate:
+        jmp .infinate
+endproc
+
+;=====================
+; is_equal
+;=====================
+proc    is_equal
+        mov ax, 1
+        pusha
+
+        ; init
+        mov si, word [bp + 4]
+        mov di, word [bp + 5]
+        mov cx, word [bp + 6]
+
 ;======================
 ; Message out function
 ;======================
@@ -109,11 +191,123 @@ proc    msg_out
         popa
 endproc
 
+;====================================
+; called by find_image function
+; if entry become "1" then call it.
+;====================================
+.image_found:
+        mov dx, word [bx+1ah]
+        mov bx, LDRSEG
+
+%define LDRSEG  02000h
+
+;====================================
+; moving 1 by 1 at root directory
+;====================================
+.find_image:
+        push 8
+        push bx
+        push str_kernel
+        call is_equal
+        add sp, 6
+        or ax, ax
+        jnz .image_found
+        add bx, 32
+        loop .find_image
+        call sys_halt
+
+str_kernel db 'RMOS BIN' 
+
+;=====================================
+; read cluster function
+;=====================================
+.read_cluster:
+        cmp dx, 0ff8h
+        jae .load_kernel
+
+        push msg_dot
+        call msg_out
+        add sp, 2
+
+        add dx, 01fh
+
+        push 00h
+        push bx 
+        push dx
+        call read_sector
+        add sp, 6
+        or ax, ax
+        jnz .increase_ldr_addr
+        call sys_halt
+
+;==================================
+; increase_ldr_addr function
+;==================================
+.increase_ldr_addr:
+        sub dx, 01fh
+        add bx, 20h     ; increase LDRSEG (512 bytes)
+
+;==================================
+; get_next_cluster function
+;==================================
+.get_next_cluster:
+        push bx
+        push dx
+        mov bx, 3
+
+        mov ax, dx
+        clc
+        rcr ax, 1
+        jc .odd_number
+
+        ; even_number
+        mul bx
+        mov bx, ax
+        mov ax, word [bx+200h]
+        and ax, 0fffh 
+        pop dx
+        pop bx
+        mov dx, ax
+        jmp .read_cluster
+
+;====================================
+; Odd number
+;====================================
+.odd_number:
+        mul bx
+        mov bx, ax
+        inc bx
+        mov ax, word [bx + 200h]
+        shr ax, 4
+        pop dx
+        pop bx 
+        mov dx, ax 
+        jmp .read_cluster
+
+;===================================
+; load kernel function
+;===================================
+.load_kernel:
+        cli
+        mov ax, LDRSEG
+        mov ds, ax
+        jmp LDRSEG:1000h
+                        ; jump to RMOS kernel
+
+;===================================
+; print message as error
+;===================================
+msg_error db 'Error!', 00h
+
 ;===================================
 ; print message as starting Loading
 ;===================================
-msg_loading     db      'Welcome to "REAL-MINESWEEPERS OPERATING SYSTEM".', 00h
-;                db      'Starting Loading...', 00h 
+msg_loading     db      'Starting RMOS...', 00h 
+
+;-------------------
+; msg_dot
+;-------------------
+msg_dot         db      '.', 00h
 
 ;-------------------
 ; boot signature
